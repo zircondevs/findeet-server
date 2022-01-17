8fbfae79217a42495544fa9b6d55228e08fc138e
Princewill Michael <yandex.code@gmail.com>
2022-01-16 20:14:59 -0500

    Minor update to pre commit and pre push hook

--- hooks/pre-push.js
+++ hooks/pre-push.js
@@ -1,21 +1,21 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const { exec } = require('./exec');
 
-exec('yarn format');
-exec('yarn lint');
+// exec('yarn format');
+// exec('yarn lint');
 
 /* The flag commit hash 🚩 */
 const stgUniqCommit = 's76o527m89e9h72619a827s9h038c029o8de',
-	currentBranch = exec('git rev-parse --abbrev-ref HEAD'),
-	/* Get all the branch names that have a commit with this hash */
-	branchesWithStaging = exec(`git branch --contains ${stgUniqCommit}`);
+  currentBranch = exec('git rev-parse --abbrev-ref HEAD'),
+  /* Get all the branch names that have a commit with this hash */
+  branchesWithStaging = exec(`git branch --contains ${stgUniqCommit}`);
 if (branchesWithStaging.includes(currentBranch)) {
-	console.log(
-		chalk.bgRed.black.bold(
-			'Your branch contains commits from \'staging\' branch.'
-		)
-	);
-	process.exit(1);
+  console.log(
+    import('chalk').bgRed.black.bold(
+      "Your branch contains commits from 'staging' branch."
+    )
+  );
+  process.exit(1);
 }
 
 /* Update local branch from origin master */
