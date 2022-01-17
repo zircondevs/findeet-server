8350cf322f4e89172eb1c1fe1c119cd1893f0a74
Princewill Michael <yandex.code@gmail.com>
2022-01-16 20:07:45 -0500

    Minor update to pre commit and pre push hook

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express, { Express } from 'express';
 import { config as envConfig } from 'dotenv';
 
 envConfig();
-const name = 'Findeet',
-	app: Express = express(),
+const name: string;
+const app: Express = express();
 
-	port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
-	console.log(`Server Running on PORT: ${port}.`);
+  console.log(`Server Running on PORT: ${port} ${name}.`);
 });
